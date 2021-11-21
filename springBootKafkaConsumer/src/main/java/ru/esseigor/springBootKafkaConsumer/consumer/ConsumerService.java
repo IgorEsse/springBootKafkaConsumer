24ad19dfc979683463dba2acbd95749c78f149bb
IgorEsse <esseigor@gmail.com>
2021-11-22 01:42:53 +0400

    Add service Kafka

---
+++ springBootKafkaConsumer/src/main/java/ru/esseigor/springBootKafkaConsumer/consumer/ConsumerService.java
@@ -0,0 +1,13 @@
+package ru.esseigor.springBootKafkaConsumer.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConsumerService {
+
+	@KafkaListener(topics = "messages", groupId = "message_group_id")
+	public void consume(String message) {
+		System.out.println("Consuming the message: " + message);
+	}
+}
